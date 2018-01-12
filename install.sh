6684880dfec762df0ee91b4b6a091e1fc94fc47e
Pengcheng Chen <pengcheng.chen@gmail.com>
2018-01-11 16:10:08 -0800

    Added install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BUNDLE_DIR=".vim/bundle"
+
+PLUGINS=" \
+    https://github.com/scrooloose/nerdtree.git \
+    https://github.com/majutsushi/tagbar.git \
+    https://github.com/junegunn/fzf.vim.git \
+    https://github.com/romainl/vim-qf.git \
+    https://github.com/vim-syntastic/syntastic.git \
+    https://github.com/fatih/vim-go.git \
+    https://github.com/tpope/vim-fugitive.git \
+"
+
+cd $BUNDLE_DIR
+
+for plugin in $PLUGINS
+    do git clone $plugin
+done
