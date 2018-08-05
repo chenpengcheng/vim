8a1b19f318636cdf9fc8583703c229bb0e17d808
Pengcheng Chen <pengcheng.chen@gmail.com>
2018-08-05 10:35:28 -0700

    Cleaned up bunble installation

--- install.sh
+++ install.sh
@@ -2,53 +2,18 @@
 
 BUNDLE_DIR=".vim/bundle"
 
-# https://github.com/w0rp/ale.git
-PLUGINS=" \
-    https://github.com/scrooloose/nerdtree.git \
-    https://github.com/majutsushi/tagbar.git \
-    https://github.com/junegunn/fzf.vim.git \
-    https://github.com/airblade/vim-rooter.git \
-    https://github.com/justinmk/vim-sneak.git \
-    https://github.com/tpope/vim-sleuth.git \
-    https://github.com/wellle/targets.vim.git \
-    https://github.com/tpope/vim-unimpaired.git \
-    https://github.com/farmergreg/vim-lastplace.git \
-    https://github.com/Valloric/YouCompleteMe.git \
-    https://github.com/godlygeek/tabular.git \
-    https://github.com/scrooloose/nerdcommenter.git \
-    https://github.com/vim-airline/vim-airline.git \
-    https://github.com/airblade/vim-gitgutter.git \
-    https://github.com/romainl/vim-qf.git \
-    https://github.com/vim-syntastic/syntastic.git \
-    https://github.com/tpope/vim-fugitive.git \
-    https://github.com/fatih/vim-go.git \
-    https://github.com/jparise/vim-graphql.git \
-    https://github.com/pangloss/vim-javascript.git \
-"
-
-mkdir -p $BUNDLE_DIR
-
-cd $BUNDLE_DIR
-for plugin in $PLUGINS
-    do git clone $plugin
-done
-cd -
+git submodule update --init --recursive
 
 cp fzf/tags.py .vim/bundle/fzf.vim/bin/
 patch -p1 < fzf/001-python-tags.patch
 patch -p1 < tagbar/001-fix-status-line-crash.patch
-patch -p1 < nerdtree/001-disable-bookmark-deletion-confirmation.patch
 cp nerdtree/custom.vim .vim/bundle/nerdtree/nerdtree_plugin/
+patch -p1 < nerdtree/001-disable-bookmark-deletion-confirmation.patch
 
-cd .vim/bundle/YouCompleteMe/
-git submodule update --init --recursive
-./install.py --clang-completer --go-completer --js-completer
-cd -
+$BUNDLE_DIR/YouCompleteMe/install.py --clang-completer --go-completer --js-completer
 
 rm -fr /tmp/.vim*
 mv $HOME/.vim* /tmp/
 cp -R .vim* $HOME/
 
-cd .vim/bundle/
-rm -fr YouCompleteMe/ ale/ fzf.vim/ nerdcommenter/ nerdtree/ tabular/ tagbar/ targets.vim/ vim-*
-cd -
+rm -fr $BUNDLE_DIR/*
